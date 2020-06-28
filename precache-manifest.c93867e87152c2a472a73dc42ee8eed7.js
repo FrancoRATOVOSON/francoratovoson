f95a8e1b2137cb32cb6e72365907fb6e53bcdea5
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2020-06-28 18:02:54 +0200

    Updates

---
+++ precache-manifest.c93867e87152c2a472a73dc42ee8eed7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c63a83fd315c7aaef5afa36d57d98a",
+    "url": "/francoratovoson/index.html"
+  },
+  {
+    "revision": "31644b749652898158eb",
+    "url": "/francoratovoson/static/css/main.ffae1438.chunk.css"
+  },
+  {
+    "revision": "69cd9b44a4da62c64990",
+    "url": "/francoratovoson/static/js/2.6b88b9c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/francoratovoson/static/js/2.6b88b9c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31644b749652898158eb",
+    "url": "/francoratovoson/static/js/main.e4f7cbb4.chunk.js"
+  },
+  {
+    "revision": "536cd62a354a1b574441",
+    "url": "/francoratovoson/static/js/runtime-main.5e0a238c.js"
+  },
+  {
+    "revision": "0ecae014578a796e9e06a4e6da9831a3",
+    "url": "/francoratovoson/static/media/Pro.0ecae014.png"
+  }
+]);
